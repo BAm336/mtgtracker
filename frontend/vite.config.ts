3052eca2db60225b1229fed81c513b506eae1c7a
bam <bam_336@hotmail.com>
2026-02-15 12:15:48 +0100

    feat: prepare deploy github pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mtgapp/',
+  base: '/',
   plugins: [
     VueRouter({
       routesFolder: 'src/pages',
